6338ecd15730ad7ce56bf58c456e1726c4b807bd
Deawalk <zgdmemail@gmail.com>
2023-09-28 20:40:09 +0800

    Feat：增加中将的血量*2

--- sql/world/updates/安其拉开门.sql
+++ sql/world/updates/安其拉开门.sql
@@ -51,3 +51,8 @@ INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `Q
 (15818, 24090, 24090, 100, 0, 1, 0, 1, 1, 'Lieutenant General Nokhor - (ReferenceTable)'),
 (15818, 24091, 24091, 100, 0, 1, 0, 1, 1, 'Lieutenant General Nokhor - (ReferenceTable)');
 
+
+DELETE FROM `creature_template` WHERE (`entry` = 15818);
+INSERT INTO `creature_template` (`entry`, `difficulty_entry_1`, `difficulty_entry_2`, `difficulty_entry_3`, `KillCredit1`, `KillCredit2`, `modelid1`, `modelid2`, `modelid3`, `modelid4`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `exp`, `faction`, `npcflag`, `speed_walk`, `speed_run`, `speed_swim`, `speed_flight`, `detection_range`, `scale`, `rank`, `dmgschool`, `DamageModifier`, `BaseAttackTime`, `RangeAttackTime`, `BaseVariance`, `RangeVariance`, `unit_class`, `unit_flags`, `unit_flags2`, `dynamicflags`, `family`, `trainer_type`, `trainer_spell`, `trainer_class`, `trainer_race`, `type`, `type_flags`, `lootid`, `pickpocketloot`, `skinloot`, `PetSpellDataId`, `VehicleId`, `mingold`, `maxgold`, `AIName`, `MovementType`, `HoverHeight`, `HealthModifier`, `ManaModifier`, `ArmorModifier`, `ExperienceModifier`, `RacialLeader`, `movementId`, `RegenHealth`, `mechanic_immune_mask`, `spell_school_immune_mask`, `flags_extra`, `ScriptName`, `VerifiedBuild`) VALUES
+(15818, 0, 0, 0, 0, 0, 15794, 0, 0, 0, 'Lieutenant General Nokhor', '', NULL, 0, 63, 63, 0, 370, 0, 4.8, 2.85714, 1, 1, 20, 1, 1, 0, 35, 2000, 2000, 1, 1, 1, 0, 2048, 0, 0, 0, 0, 0, 0, 10, 8, 15818, 0, 0, 0, 0, 0, 0, 'SmartAI', 0, 1, 300, 1, 1, 1, 0, 228, 1, 646659935, 0, 1, '', 12340);
+
